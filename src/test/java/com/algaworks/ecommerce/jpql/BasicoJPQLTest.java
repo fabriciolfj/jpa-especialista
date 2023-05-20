50533bdb335c3696a061070ff9ab6c9ea6a7c073
fabricio jacob <fabrluc@outlook.com>
2023-05-19 21:48:55 -0300

    add

--- src/test/java/com/algaworks/ecommerce/jpql/BasicoJPQLTest.java
+++ src/test/java/com/algaworks/ecommerce/jpql/BasicoJPQLTest.java
@@ -1,13 +1,18 @@
 package com.algaworks.ecommerce.jpql;
 
 import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.model.Cliente;
 import com.algaworks.ecommerce.model.Pedido;
+import com.algaworks.ecommerce.model.Produto;
 import org.junit.Test;
 
 import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 
+import java.util.List;
+
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 public class BasicoJPQLTest extends EntityManagerTest {
 
@@ -33,5 +38,22 @@ public class BasicoJPQLTest extends EntityManagerTest {
 
         final Pedido pedido = (Pedido) query.getSingleResult();
         assertNotNull(pedido);
+    }
+
+    @Test
+    public void selectionarUmAtributoParaRetorno() {
+        final var jqpl = "select p.nome from Produto p";
+
+        final TypedQuery<String> query = entityManager.createQuery(jqpl, String.class);
+        var names = query.getResultList();
+
+        assertTrue(names.size() > 1);
+
+        final TypedQuery<Cliente> queryClient = entityManager.createQuery("select p.cliente from Pedido p where p.id = 7", Cliente.class);
+        final var cliente = queryClient.getSingleResult();
+
+        assertNotNull(cliente);
+        assertTrue(Cliente.class.equals(cliente.getClass()));
+
     }
 }
