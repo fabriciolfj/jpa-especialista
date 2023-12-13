d7ec8d1289af612a509d9c759703bffe795f789c
fabricio jacob <fabrluc@outlook.com>
2023-12-12 22:35:23 -0300

    add

--- src/test/java/com/algaworks/ecommerce/criteria/BasicoCriteriaTest.java
+++ src/test/java/com/algaworks/ecommerce/criteria/BasicoCriteriaTest.java
@@ -1,16 +1,69 @@
 package com.algaworks.ecommerce.criteria;
 
 import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.model.Cliente;
 import com.algaworks.ecommerce.model.Pedido;
+import com.algaworks.ecommerce.model.Produto;
+import com.mysql.cj.xdevapi.Client;
 import org.junit.Assert;
 import org.junit.Test;
 
+import javax.persistence.Tuple;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
 
 public class BasicoCriteriaTest extends EntityManagerTest {
+
+    @Test
+    public void projetarResultadoTuple() {
+        final CriteriaBuilder builder = this.entityManager.getCriteriaBuilder();
+        final CriteriaQuery<Tuple> query = builder.createTupleQuery();
+        final Root<Produto> root = query.from(Produto.class);
+
+        query.select(builder.tuple(root.get("id").alias("id"), root.get("nome").alias("nome")));
+
+        var type = this.entityManager.createQuery(query);
+        var result = type.getResultList();
+
+        Assert.assertNotNull(result);
+        Assert.assertFalse(result.isEmpty());
+
+        result.forEach(s -> System.out.println("id " + s.get("id") + " nome " + s.get("nome")));
+    }
+
+    @Test
+    public void projetarResultado() {
+        final CriteriaBuilder builder = this.entityManager.getCriteriaBuilder();
+        final CriteriaQuery<Object[]> query = builder.createQuery(Object[].class);
+        final Root<Produto> root = query.from(Produto.class);
+
+        query.multiselect(root.get("id"), root.get("nome"));
+
+        var type = this.entityManager.createQuery(query);
+        var result = type.getResultList();
+
+        Assert.assertNotNull(result);
+        Assert.assertFalse(result.isEmpty());
+
+        result.forEach(s -> System.out.println("id " + s[0] + " nome " + s[1]));
+    }
+
+    @Test
+    public void selecionarUmAtributoDiferente() {
+        final CriteriaBuilder builder = this.entityManager.getCriteriaBuilder();
+        final CriteriaQuery<Cliente> query = builder.createQuery(Cliente.class);
+        final Root<Pedido> root = query.from(Pedido.class);
+
+        query.select(root.get("cliente"));
+        query.where(builder.equal(root.get("id"), 7));
+
+        var type = this.entityManager.createQuery(query);
+        var result = type.getSingleResult();
+
+        Assert.assertNotNull(result);
+    }
 
     @Test
     public void buscarPorIdentificador() {
